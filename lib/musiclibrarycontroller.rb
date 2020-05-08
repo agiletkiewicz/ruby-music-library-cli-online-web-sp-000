665bbb6722150982b8066add36f32c859df0b309
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-08 07:40:44 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -77,6 +77,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     index = gets.strip.to_i - 1
+    binding.pry
     sorted_songs = Song.all.sort_by { |song| song.name}
     if index <= Song.all.size
       puts "Playing #{sorted_songs[index].name} by #{sorted_songs[index].artist.name}"
